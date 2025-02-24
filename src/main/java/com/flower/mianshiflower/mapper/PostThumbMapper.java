e3bd624bb420c5271e241ba8b86b46eaa067160c
kaseketsu <755740189@qq.com>
2025-02-24 22:36:17 +0800

    first commit

---
+++ src/main/java/com/flower/mianshiflower/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.flower.mianshiflower.mapper;
+
+import com.flower.mianshiflower.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author <a href="https://github.com/liflower">程序员鱼皮</a>
+ * @from <a href="https://flower.icu">编程导航知识星球</a>
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/flower/mianshiflower/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,30 @@
+package com.flower.mianshiflower.model.dto.postfavour;
+
+import com.flower.mianshiflower.common.PageRequest;
+import com.flower.mianshiflower.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ *
+ * @author <a href="https://github.com/liflower">程序员鱼皮</a>
+ * @from <a href="https://flower.icu">编程导航知识星球</a>
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
